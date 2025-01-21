2d63b6b1aac294d804d57a2bdfc0acaf94b6d570
dipesh1186 <dipeshmali1221@gmail.com>
2025-01-21 22:57:59 +0530

    dd brach added

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './app.tsx'
+import Banner from './homepage/banner.tsx'
+import Footer from './homepage/footer.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Banner />
+    <Footer />
   </StrictMode>,
 )
