033aae430bb3f0f17a4bfdec66af8d74998c538e
“Hani239” <hanivinaben2924@gmail.com>
2025-01-21 18:26:36 +0530

    Product Page Updated

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,9 +4,18 @@ import './index.css'
 import App from './App.tsx'
 import Product from './Product.tsx'
 
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <App /> */}
-    <Product />
+    <Router>
+            {/* <nav>
+                <Link to="/">Main Page</Link> | <Link to="/product">Product Page</Link>
+            </nav> */}
+            <Routes>
+                <Route path="/" element={<App />} />
+                <Route path="/product" element={<Product />} />
+            </Routes>
+        </Router>
   </StrictMode>,
 )
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,6 +7,7 @@ export default {
     extend: {
       fontFamily: {
         marcellus: ['Marcellus'],
+        nunito: ['Nunito'],
       },
     },
   },
